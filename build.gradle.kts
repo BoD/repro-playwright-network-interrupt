eadde4480bd009f44f18a16c50c191ad4f564a20
BoD <BoD@JRAF.org>
2024-12-07 18:05:12 +0100

    Add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    application
 }
 
 group = "com.example"
@@ -12,6 +13,10 @@ repositories {
 dependencies {
     implementation("com.microsoft.playwright:playwright:1.44.0") // WORKS
 //    implementation("com.microsoft.playwright:playwright:1.45.0") // DOES NOT WORK
+}
+
+application {
+    mainClass.set("com.example.MainKt")
 }
 
 kotlin {
